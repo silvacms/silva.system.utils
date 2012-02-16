b1a47d34dd7f6e7a11bf3a24db69b4a0a4a21952
Sylvain Viollon <thefunny@gmail.com>
2012-02-16 16:33:10 +0100

    Copy walk_silva_tree from Silva 3.0.

--- src/silva/system/utils/commands/sources.py
+++ src/silva/system/utils/commands/sources.py
@@ -1,12 +1,33 @@
 
 import logging
 
-from Products.SilvaExternalSources.interfaces import ICodeSource
 from Products.SilvaDocument.interfaces import IDocument
-from silva.core.services.utils import walk_silva_tree
+from Products.SilvaExternalSources.interfaces import ICodeSource
+from silva.core.interfaces import ISilvaObject, IContainer
 from silva.system.utils.script import NEED_SILVA_SESSION
 
 logger = logging.getLogger('silva.system')
+
+
+THRESHOLD = 1000
+
+def walk_silva_tree(content, requires=ISilvaObject):
+    """A generator to lazily get all the Silva object from a content /
+    container.
+    """
+    count = 0
+    if requires.providedBy(content):
+        # Version are indexed by the versioned content itself
+        yield content
+    if IContainer.providedBy(content):
+        for child in content.objectValues():
+            for content in walk_silva_tree(child):
+                count += 1
+                yield content
+                if count > THRESHOLD:
+                    # Review ZODB cache
+                    content._p_jar.cacheGC()
+                    count = 0
 
 
 class FindSourcesCommand(object):
@@ -36,11 +57,10 @@ class FindSourcesCommand(object):
         logger.info("Finding sources in '%s'." % (
                 '/'.join(root.getPhysicalPath())))
         sources = 0
-        for content in walk_silva_tree(root):
-            if ICodeSource.providedBy(content):
-                logger.warn("Found source '%s'." % (
-                        '/'.join(content.getPhysicalPath())))
-                sources += 1
+        for source in walk_silva_tree(root, requires=ICodeSource):
+            logger.warn("Found source '%s'." % (
+                    '/'.join(source.getPhysicalPath())))
+            sources += 1
         logger.info('Found %d sources.' % sources)
 
     def find_document_sources(self, root, identifier):
@@ -48,12 +68,10 @@ class FindSourcesCommand(object):
                 identifier,
                 '/'.join(root.getPhysicalPath())))
         documents = 0
-        for content in walk_silva_tree(root):
-            if not IDocument.providedBy(content):
-                continue
+        for document in walk_silva_tree(root, requires=IDocument):
             documents += 1
-            self.inspect_document(content.get_viewable(), identifier)
-            self.inspect_document(content.get_editable(), identifier)
+            self.inspect_document(document.get_viewable(), identifier)
+            self.inspect_document(document.get_editable(), identifier)
         logger.info('Verified %d documents.' % documents)
 
     def inspect_document(self, version, identifier):
